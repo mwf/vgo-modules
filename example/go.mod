f5cfa819598f0fcc29d7ae2347cc18c09b50cfee
Ivan Korolev <i.d.korolev@gmail.com>
2018-07-06 18:54:42 +0300

    add `example` module

---
+++ example/go.mod
@@ -0,0 +1,3 @@
+module github.com/mwf/vgo-modules/example
+
+require github.com/mwf/vgo-modules v0.0.2
---
+++ example/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mwf/vgo-modules"
+	"github.com/mwf/vgo-modules/a"
+)
+
+func main() {
+	fmt.Printf("vgo-modules version: %s\n", vgo_modules.Version)
+	fmt.Printf("a.A: %s\n", a.A)
+}

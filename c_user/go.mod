8606afa2459e362e58f6e02c3d85c833447ccb75
Ivan Korolev <i.d.korolev@gmail.com>
2018-07-19 19:13:30 +0300

    add a main module using C

---
+++ c_user/go.mod
@@ -0,0 +1,3 @@
+module github.com/mwf/vgo-modules/c_user
+
+require github.com/mwf/vgo-modules/c v0.1.0
---
+++ c_user/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mwf/vgo-modules/c"
+)
+
+func main() {
+	fmt.Println("CA:", c.CA)
+}

3c1ba2c013ff8747b780d8d658338249d95ac4e5
Ivan Korolev <i.d.korolev@gmail.com>
2018-07-19 17:02:18 +0300

    use `a` in main project

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/mwf/vgo-modules
+
+require github.com/mwf/vgo-modules/a v0.0.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mwf/vgo-modules/a"
+)
+
+func main() {
+	fmt.Println("A:", a.A)
+	fmt.Println("AB:", a.AB)
+}

68531cc387fcc52a2888f0abde0c9f59f3a3ecbb
Ivan Korolev <i.d.korolev@gmail.com>
2018-07-19 17:05:47 +0300

    a: remove dependency from `b`

--- a/a.go
+++ a/a.go
@@ -1,7 +1,5 @@
 package a
 
-import "github.com/mwf/vgo-modules/b"
-
 const A = "A"
 
-const AB = A + b.B
+const AB = A + "B"
--- a/go.mod
+++ a/go.mod
@@ -1,3 +1,1 @@
 module github.com/mwf/vgo-modules/a
-
-require github.com/mwf/vgo-modules/b v0.0.1
